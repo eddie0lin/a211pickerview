d1b5ca1335d054627942210a4dc6453a35291ff6
Cathy <cathy.lin.0112@gmail.com>
2020-11-21 16:40:47 +0800

    PickerView

--- a211pickerview/ViewController.swift
+++ a211pickerview/ViewController.swift
@@ -7,13 +7,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate {
 
+
+    @IBOutlet weak var pickerView: UIPickerView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        pickerView.dataSource = self
+        pickerView.delegate = self
+        
+        
+    }
+    
+    //MARK: PickerView DataSource
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
     }
 
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        switch component {
+        case 0:
+            return 5
+        case 1:
+            return 8
+        default:
+            return 0
+        }
+        
+    }
+    //MARK: PickerView Delegate
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return "column:\(component),row:\(row)"
+    }
 
 }
 
